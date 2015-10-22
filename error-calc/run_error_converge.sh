4b34c7af65c98cce8e85862e9c709ddbfd035e2a
Eric Jalbert <ericmjalbert@gmail.com>
2015-10-22 14:16:15 -0400

    Decoupled run_error
    It now will detect which output number to use (the second last) and
    which files to use.

--- error-calc/run_error_converge.sh
+++ error-calc/run_error_converge.sh
@@ -6,28 +6,32 @@ resu=`find ~/Documents -name batch-results -type d`
 cp -r $resu/out-p* ./
 
 ## need to get the smallest pNum for counting
-#pNum=$(echo out-p* |\
-#       sed 's/[^0-9 ]//g' |\
-#       fmt -1 |\
-#       sort -n |\
-#       awk '{ if (NR==1) print $0}')
-#
-#for dir in out-p*
-#do
-#  mv $i/2D_out10.dat
-#  # want something like
-#  # mv "$dir"/2D_out"$last" "$pNum".tmp
-#  pNum=$(bc <<< "$pNum*2-1")
-#done
+pNum=$(echo out-p* |\
+       sed 's/[^0-9 ]//g' |\
+       fmt -1 |\
+       sort -n |\
+       head -n 1)
 
-mv out-p33/2D_out10.dat 33.tmp
-mv out-p65/2D_out10.dat 65.tmp
-mv out-p129/2D_out10.dat 129.tmp
-mv out-p257/2D_out10.dat 257.tmp
-mv out-p513/2D_out10.dat 513.tmp
-mv out-p1025/2D_out10.dat 1025.tmp
-mv out-p2049/2D_out10.dat 2049.tmp
-mv out-p4097/2D_out10.dat 4097.tmp
+# Grabs the highest numbered output.
+#   echo gives out-p123/2D_out456.dat so the first sed gets rid 
+#   of everything before the last 456. The second sed grabs 
+#   remaining numbers. Because we want last num, sort in reverse
+#   Note that the last out is corrupt, so use head and tail to 
+#   grab the second to last output
+last=$(echo out-p$pNum/2D_out* |\
+       sed 's#out-p[0-9]*/\(2D_out\|out\)##g' |\
+       sed 's#[^0-9 ]##g' |\
+       fmt -1 |\
+       sort -nr |\
+       head -n 2|\
+       tail -n 1)
+
+for dir in out-p*
+do
+  mv "$dir"/2D_out"$last".dat "$pNum".tmp
+  pNum=$(bc <<< "$pNum*2-1")
+done
+
 
 Rscript norm_vs_best.R
 
